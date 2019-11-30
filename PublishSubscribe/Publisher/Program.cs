4522bb6886d78964b4ba410ea35d3d11fbe186e6
Van Khanh Le <levankhanhtpd@gmail.com>
2019-11-30 20:34:14 +0700

    Add Publish/Subscribe

---
+++ PublishSubscribe/Publisher/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        private static IConnection connection;
+        private static IModel channel;
+        static void Main(string[] args)
+        {
+            RegisterPublisher();
+            var message = "";
+            while (message.ToLower() != "cancel")
+            {
+                Console.WriteLine(" Enter [cancel] to exit.");
+                Console.WriteLine(" Please enter a number of message(s) will be send (n >= 1).");
+                int totalMessage;
+                if (int.TryParse(Console.ReadLine(), out totalMessage))
+                {
+                    Console.WriteLine(" Please enter a message to send.");
+                    message = Console.ReadLine();
+                    for (int i = 1; i <= totalMessage; i++)
+                    {
+                        Publish($"Message index {i}: {message}");
+                    }
+                }
+            }
+        }
+
+        private static void RegisterPublisher()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+        }
+
+        private static void Publish(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs",
+                                   routingKey: "",
+                                   basicProperties: null,
+                                   body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+}
