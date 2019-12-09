2064e81d2f7b5a68fb27e65b15a2be02f8471413
Van Khanh LE <vle2@amaris.com>
2019-12-09 09:41:34 +0700

    add projects Topics

---
+++ Topics/Publisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+
+        static IModel channel;
+        static IConnection connection;
+
+        static void Main()
+        {
+            RegisterPublisher();
+
+            while (true)
+            {
+                Console.WriteLine(" Do you want to continue?(y/n):");
+                var continueKey = Console.ReadLine();
+
+                if (string.Equals(continueKey, "n", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                Console.WriteLine(" Please enter a routingKey.");
+                var routingKey = Console.ReadLine();
+                Console.WriteLine(" Please enter a message.");
+                var message = Console.ReadLine();
+                Publish(routingKey, message);
+            }
+        }
+
+        private static void RegisterPublisher()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+        }
+
+        private static void Publish(string routingKey, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "topic_logs",
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+        }
+    }
+}
